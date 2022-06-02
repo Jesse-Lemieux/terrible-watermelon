4643bb03eeb8d32d6dc839982ed121d56222e8b6
JESSE <dtrsjesse121@gmail.com>
2022-06-02 15:37:49 -0400

    email validation and error message handling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm(){
 
+const [errorMessage, setErrorMessage] = useState('');
 const [formState, setFormState] = useState({ name: '', email: '', message: ''});
 const { name, email, message } = formState;
 
 function handleChange(e) {
-    setFormState({...formState, [e.target.name]: e.target.value })
+    if (e.target.name === 'email') {
+        const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+              setErrorMessage(`${e.target.name} is required.`);
+            } else {
+              setErrorMessage('');
+            }
+          }  
+    console.log('errorMessage', errorMessage);
+    if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+      }
   }
 
 function handleSubmit(e) {
